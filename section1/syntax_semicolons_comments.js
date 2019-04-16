ca9aba05dd6af7690fe2d532ee29e1630ab680b8
David Katz <15Dkatz@shcp.edu>
2019-04-15 21:04:34 -0700

    Add syntax - semicolons and comments lecture

---
+++ section1/syntax_semicolons_comments.js
@@ -0,0 +1,15 @@
+// semicolons are optional, but recommended after statements
+var foo = 10;
+var bar = 17;
+
+// Comments:
+
+// single:
+// bears, beets, battlestar galactica
+
+// multiline
+/**
+ * bears,
+ * beets,
+ * battlestar galactica
+*/
