e35f9fab9a2f126443dfb5fa417844f7c3760b32
David Katz <15Dkatz@shcp.edu>
2019-04-15 20:00:37 -0700

    Add section 1

---
+++ section1/console_operators_variables.js
@@ -0,0 +1,13 @@
+// Numbers
+1, 2, 999
+
+// Strings
+"dog", 'dog'
+"cat", 'cat'
+// notice that double or single quotes are interchangeable
+
+// Read-eval-print-loop. Four step program:
+// 1. Read user input.
+// 2. Evaluate that input.
+// 3. Print the result
+// 4. Loop and repeat steps 1-3.
