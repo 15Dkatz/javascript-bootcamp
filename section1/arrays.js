5680ecd5a471f61415963c4ccd174dd690b692ae
David Katz <15Dkatz@shcp.edu>
2019-04-15 21:39:13 -0700

    Add arrays

---
+++ section1/arrays.js
@@ -0,0 +1,12 @@
+var things = ['laptop', 'speaker', 13];
+
+things.length;
+
+// Add an item to the array
+things.push(17);
+
+// .splice(n, x, ...)
+// Starting from index n, remove x items.
+// Add additional arguments to the array
+things.splice(1, 0, 'pen', 'paper');
+
