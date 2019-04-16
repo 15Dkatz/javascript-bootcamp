7e740b8def85de312282e4293f52969357d875a7
David Katz <15Dkatz@shcp.edu>
2019-04-15 21:52:29 -0700

    Add objects

---
+++ section1/objects.js
@@ -0,0 +1,14 @@
+var lion = {
+  name: 'simba',
+  age: 3
+};
+
+// name and age are properties of the lion object.
+
+// dot notation.
+lion.age;
+
+// bracket notation.
+// This is useful if you're accessing a property through a variable
+var prop = 'name';
+lion[prop];
