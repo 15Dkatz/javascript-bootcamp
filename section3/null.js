5590cbdd125cb5062182d0f49336909a3a378490
David Katz <15Dkatz@shcp.edu>
2019-04-18 20:35:51 -0700

    Add null

---
+++ section3/null.js
@@ -0,0 +1,16 @@
+// Null
+// Represents the absence of an object
+
+var eagle = null;
+// represents the fact that eagle will be set to an object later
+
+typeof null; // object
+
+// Strict equality with undefined.
+// They have different types.
+// So it should be false
+null === undefined // false.
+
+// Loose equality with undefined.
+// JavaScript coerces null to undefined.
+null === undefined // true.
