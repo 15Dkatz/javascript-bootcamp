668aefb7d33adcd12aaf58552483ad393b2c2b5e
David Katz <15Dkatz@shcp.edu>
2019-04-22 23:17:03 -0700

    Add interview question on callbacks

---
+++ section2/interview_question_callbacks.js
@@ -0,0 +1,25 @@
+// write functions `logMessage` and `greet`
+// such that the following line logs 'hi'
+
+logMessage(greet(() => greet(() => 'hi')));
+
+// *** Answer ***
+
+function logMessage(message) {
+  console.log(message);
+}
+
+function greet(callback) {
+  return callback();
+}
+
+// var returnHi = () => 'hi';
+// greet(returnHi);
+
+// no need for the returnHi variable. Pass the callback inline
+// greet(() => 'hi'); // 'hi'
+
+// Notice how the input to logMessage in the question can be broken down:
+// greet(() => greet(() => 'hi'));
+// greet(() => 'hi');
+// 'hi'
